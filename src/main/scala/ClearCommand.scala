514d7fb075a1530a6f51390fc95034466d616416
sky0621 <sky14work@gmail.com>
2019-09-20 08:29:53 +0900

    continue

--- src/main/scala/ClearCommand.scala
+++ src/main/scala/ClearCommand.scala
@@ -1,8 +1,14 @@
+import java.io.IOException
+import java.nio.file.{Files, Paths}
+
 // privateにすることで new による生成を抑止
 class ClearCommand private(storeInfo: StoreInfo) extends Command {
   override def exec(args: Array[String]): Unit = {
-    //    FIXME: logic
-    println("CLEAR")
+    try {
+      Files.write(Paths.get(storeInfo.storeName), "{}".getBytes())
+    } catch {
+      case e: IOException => println(e.getMessage)
+    }
   }
 }
 
--- src/main/scala/Commands.scala
+++ src/main/scala/Commands.scala
@@ -1,31 +1,34 @@
+import java.io.PrintWriter
 import java.nio.file.{Files, Paths}
 
 // privateにすることで new による生成を抑止
 class Commands private(commands: Map[String, Command]) {
   def exec(cmds: Array[String]): Unit = {
-    if (cmds == null || cmds.size < 1) {
+    if (cmds == null || cmds.length < 1) {
       print("no target")
       return
     }
     val cmdName = cmds.head
     val cmdArgs = cmds.tail
-    commands.get(cmdName).get.exec(cmdArgs)
+    commands(cmdName).exec(cmdArgs)
   }
 }
 
 // コンパニオンオブジェクト（Commandsクラスのコンストラクタを隠蔽するファクトリ）
 object Commands {
   def apply(storeInfo: StoreInfo): Commands = {
-    val commands = Map(
+    val store = Paths.get(storeInfo.storeName)
+    if (Files.notExists(store)) {
+      try(Files.createFile(store))
+      val pw = new PrintWriter(storeInfo.storeName)
+      pw.write("{}")
+      pw.close()
+    }
+
+    new Commands(Map(
       "end" -> EndCommand(),
       "help" -> HelpCommand(),
       "clear" -> ClearCommand(storeInfo),
-    )
-
-    if (Files.notExists(Paths.get(storeInfo.storeName))) {
-      commands.get("clear").get.exec(null)
-    }
-
-    new Commands(commands)
+    ))
   }
 }
